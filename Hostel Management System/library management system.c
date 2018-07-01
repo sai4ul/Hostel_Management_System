4862466d52857ac3a78b270986a0ac22ffc6fd30
SAIFUL ISLAM <23151213+sai4ul@users.noreply.github.com>
2018-07-01 16:29:59 +0600

    This my Last Update project

--- Hostel Management System/library management system.c
+++ Hostel Management System/library management system.c
@@ -1,148 +1,125 @@
-//BUBT Hostel Management System
-//Password: bubt
+//Hostel Management system
+// Password: bubt
 
 #include<stdio.h>
+#include<string.h>
 #include<windows.h>
-#include <stdlib.h>
-#include<string.h>                  //contains strcmp(),strcpy(),strlen(),etc
-#include<dos.h>                     //contains _dos_getdate
+#include<ctype.h>
 
-
-                        //password bubt
-char catagories[][15]={"Fllor one","Fllor two","Fllor three"};
-
-
+char Floor_No[][15]={"1st Floor","2nd Floor","3rd Floor","4th Floor","5th Floor","6th Floor"};
 void returnfunc(void);
 void mainmenu(void);
-void addbooks(void);
-void deletebooks(void);
-void editbooks(void);
-void searchbooks(void);
-void issuebooks(void);
-void viewbooks(void);
+void add_student(void);
+void Remove_student(void);
+void viewstd(void);
 void closeapplication(void);
 int  getdata();
 int  checkid(int);
-int t(void);
-//void show_mouse(void);
 void Password();
-void issuerecord();
 void loaderanim();
 
-//list of global files that can be acceed form anywhere in program
+
 FILE *fp,*ft,*fs;
-
-
 COORD coord = {0, 0};
 
-
-
-//list of global variable
 int s;
 char findbook;
 char password[10]={"bubt"};
-
+                                // X and Y coordinates(Password mid e state kora)
 void gotoxy (int x, int y)
 {
-    coord.X = x; coord.Y = y; // X and Y coordinates
+    coord.X = x; coord.Y = y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
-struct meroDate
-{
-    int mm,dd,yy;
-};
+
 struct books
 {
     int id;
     char stname[20];
     char name[20];
-    char Author[20];
-
-
-    int quantity;
-    float Price;
-
+    char sit_number[20];
+    int Room_No;
+    int Advance;
     int count;
     int rackno;
-
     char *cat;
-
-    struct meroDate issued;
-    struct meroDate duedate;
 };
 
-
 struct books a;
-main()
+int main()
 {
     Password();
     getch();
+    return 0;
 }
-    void mainmenu()
-    {
+
+
+void mainmenu()
+{
     system("cls");
     int i;
-    gotoxy(20,5);
-    printf("1. Add student   ");
+    gotoxy(20,3);
+    printf(" Bangladesh University Of Business Technology");
 
+
+    gotoxy(20,5);
+    printf("\t\t 1. Add Student");
     gotoxy(20,7);
-    printf("2. Delete");
+    printf("\t\t 2. Remove Student list");
 
     gotoxy(20,9);
-    printf("3. View Book list");
-
+    printf("\t\t 3. View Student list");
+    gotoxy(20,20);
     t();
-    gotoxy(20,21);
-    printf("Enter your choice:");
 
 switch(getch())
-{
+    {
         case '1':
-        addbooks();
+        add_student();
         break;
         case '2':
-        deletebooks();
+        Remove_student();
         break;
 
         case '3':
-        viewbooks();
+        viewstd();
         break;
-        case '6':
-default:
+        default:
         {
             gotoxy(10,23);
-            printf("Wrong Password!!");
-
+            printf("\t  Wrong Entry!!!!");
             if(getch())
             mainmenu();
         }
-}
+    }
 }
 
-       //Add of the book
-
-void addbooks(void)    //funtion that add books
+void add_student(void)                      //funtion that add student in floor
 {
+    system("cls");
     int i;
-
+    gotoxy(20,5);
+    printf("Select Floor in Hostel");
     gotoxy(20,7);
-    printf("1. First Fllor");
+    printf("\t  1. 1st Floor");
     gotoxy(20,9);
-    printf("2. Second Fllor");
+    printf("\t  2. 2nd Floor");
     gotoxy(20,11);
-    printf("3. Third Fllor");
+    printf("\t  3. 3rd Floor");
     gotoxy(20,13);
-    printf("4. Fourth Fllor");
+    printf("\t  4. 4th Floor");
     gotoxy(20,15);
-    printf("5. Fifth Fllor");
+    printf("\t  5. 5th Floor");
+
     gotoxy(20,17);
-    printf("6. Seven Fllor");
+    printf("\t  6. 6th Floor");
+
     gotoxy(20,19);
-    printf("7. Back");
+    printf("\t  7. Back to main menu");
 
-
-    printf("Enter your choice:");
+    gotoxy(20,22);
+    printf("Enter your \:");
     scanf("%d",&s);
     if(s==7)
 
@@ -151,22 +128,26 @@ void addbooks(void)    //funtion that add books
     fp=fopen("sai4ul.txt","ab+");
     if(getdata()==1)
     {
-        a.cat=catagories[s-1];
+        a.cat=Floor_No[s-1];
         fseek(fp,0,SEEK_END);
         fwrite(&a,sizeof(a),1,fp);
         fclose(fp);
         gotoxy(21,14);
         printf("The record is sucessfully saved");
 
+                                                    //save command
+        gotoxy(21,15);
+        printf("Save any more?(Y / N):");
         if(getch()=='n')
-        mainmenu();
+            mainmenu();
         else
-        system("cls");
-        addbooks();
+            system("cls");
+            add_student();
     }
 }
 
-void deletebooks()    //function that delete items from file fp
+
+void Remove_student()    //function that delete items from file fp
 {
     system("cls");
     int d;
@@ -175,64 +156,63 @@ void deletebooks()    //function that delete items from file fp
     {
         system("cls");
         gotoxy(10,5);
-        printf("Enter the Book ID to  delete:");
+        printf("Enter Student ID:");
         scanf("%d",&d);
         fp=fopen("sai4ul.txt","rb+");
         rewind(fp);
         while(fread(&a,sizeof(a),1,fp)==1)
         {
-            if(a.id==d)
-            {
-                gotoxy(10,7);
-                printf("The book record is available");
-                gotoxy(10,8);
-                printf("Book name is %s",a.name);
-                gotoxy(10,9);
-                printf("Rack No. is %d",a.rackno);
-                findbook='t';
-            }
+        if(a.id==d)
+        {
+            gotoxy(10,7);
+            printf("Student record is available");
+            gotoxy(10,8);
+            printf("Student Name is %s",a.name);
+            gotoxy(10,9);
+            printf("Rack No. is %d",a.rackno);
+            findbook='t';
         }
-
+        }
         if(findbook!='t')
         {
             gotoxy(10,10);
-            printf("No record is found modify the search");
+            printf("Not Student ID  found!!!");
             if(getch())
             mainmenu();
         }
         if(findbook=='t' )
         {
-            gotoxy(10,9);
-            printf("Do you want to delete it?(Y/N):");
-            if(getch()=='y')
+        gotoxy(10,9);
+        printf("Do you want to delete it?(Y/N):");
+        if(getch()=='y')
+        {
+            ft=fopen("test.dat","wb+");  //temporary file for delete
+            rewind(fp);
+            while(fread(&a,sizeof(a),1,fp)==1)
             {
-                ft=fopen("test.dat","wb+");  //temporary file for delete
-                rewind(fp);
-                while(fread(&a,sizeof(a),1,fp)==1)
+                if(a.id!=d)
                 {
-                    if(a.id!=d)
-                    {
-                    fseek(ft,0,SEEK_CUR);
-                    fwrite(&a,sizeof(a),1,ft); //write all in tempory file except that
-                    }                              //we want to delete
-                }
-                fclose(ft);
-                fclose(fp);
-                remove("sai4ul.txt");
-                rename("test.dat","sai4ul.txt"); //copy all item from temporary file to fp except that
-                fp=fopen("sai4ul.txt","rb+");    //we want to delete
-                if(findbook=='t')
-                {
-                    gotoxy(10,10);
-                    printf("The record is sucessfully deleted");
-                    gotoxy(10,11);
-                    printf("Delete another record?(Y/N)");
-                }
+                fseek(ft,0,SEEK_CUR);
+                fwrite(&a,sizeof(a),1,ft); //write all in tempory file except that
+                }                              //we want to delete
             }
-            else
-            mainmenu();
-            fflush(stdin);
-            another=getch();
+            fclose(ft);
+            fclose(fp);
+            remove("sai4ul.txt");
+            rename("test.dat","sai4ul.txt"); //copy all item from temporary file to fp except that
+            fp=fopen("sai4ul.txt","rb+");    //we want to delete
+            if(findbook=='t')
+            {
+                gotoxy(10,10);
+                printf("Record is sucessfully deleted");
+                gotoxy(10,11);
+                printf("Delete another record?(Y/N)");
+            }
+        }
+        else
+        mainmenu();
+        fflush(stdin);
+        another=getch();
         }
     }
     gotoxy(10,15);
@@ -240,42 +220,43 @@ void deletebooks()    //function that delete items from file fp
 }
 
 
-void viewbooks(void)  //show the list of book persists in library
+
+void viewstd(void)  //show student list
 {
     int i=0,j;
     system("cls");
     gotoxy(1,1);
-    printf("            Bangladesh University of Business Technology    ");
-    gotoxy(2,2);
-    printf("\n");
+    printf("                Hostel Management System  Student List      ");
+    gotoxy(2,2);//enter add
     gotoxy(3,3);
-    printf("   Fllor_No        ID    Student_Name     AUTHOR       QTY     PRICE ");
+    printf(" Floor NO.    ID      Name      Sit No.      Room NO   Advance TK");
+
     j=4;
     fp=fopen("sai4ul.txt","rb");
     while(fread(&a,sizeof(a),1,fp)==1)
     {
-    gotoxy(3,j);
-    printf("%s",a.cat);
-    gotoxy(16,j);
-    printf("%d",a.id);
-    gotoxy(22,j);
-    printf("%s",a.name);
-    gotoxy(36,j);
-    printf("%s",a.Author);
-    gotoxy(50,j);
-    printf("%d",a.quantity);
-    gotoxy(57,j);
-    printf("%.2f",a.Price);
-    gotoxy(69,j);
-    printf("%d",a.rackno);
-    printf("\n\n");
-    j++;
-    i=i+a.quantity;
+        gotoxy(3,j);
+        printf("%s",a.cat);
+        gotoxy(16,j);
+        printf("%d",a.id);
+        gotoxy(22,j);
+        printf("%s",a.name);
+        gotoxy(36,j);
+        printf("%s",a.sit_number);
+
+        gotoxy(50,j);
+        printf("%d",a.Room_No);
+
+        gotoxy(57,j);
+        printf("%d",a.Advance);
+        printf("\n\n");
+        j++;
+        i=i+a.Room_No;
     }
-    gotoxy(3,25);
-    printf("Total Books =%d",i);
-    fclose(fp);
-    gotoxy(35,25);
+    //gotoxy(3,25);
+    //printf("Total Books =%d",i);
+    //fclose(fp);
+   gotoxy(35,25);
     returnfunc();
 }
 
@@ -291,14 +272,13 @@ void returnfunc(void)
     else
     goto a;
 }
-
-                    // add students
+                               //Enter student information
 int getdata()
 {
     int t;
-    gotoxy(20,5);
+    gotoxy(20,3);printf("Enter student information");
+
     gotoxy(46,5);
-    gotoxy(20,6);
     gotoxy(46,6);
     gotoxy(20,7);
     gotoxy(46,7);
@@ -310,42 +290,45 @@ int getdata()
     gotoxy(46,10);
     gotoxy(20,11);
     gotoxy(46,11);
+
     gotoxy(21,5);
-
-    printf("Category:");
+    printf("Floor No:");
     gotoxy(31,5);
-
-    printf("%s",catagories[s-1]);
+    printf("%s",Floor_No[s-1]);
     gotoxy(21,6);
-    printf("STD ID:\t");
+    printf("Std ID:");
     gotoxy(30,6);
     scanf("%d",&t);
     if(checkid(t) == 0)
     {
-    gotoxy(21,13);
-    printf("\a Student id exists\a");
-    getch();
-    mainmenu();
-    return 0;
+        gotoxy(21,13);
+        printf("\t Student id already exists");
+        getch();
+        mainmenu();
+        return 0;
     }
     a.id=t;
     gotoxy(21,7);
-    printf("STD Name:");gotoxy(33,7);
+    printf("std Name:");gotoxy(33,7);
     scanf("%s",a.name);
+
     gotoxy(21,8);
-    printf("Author:");gotoxy(30,8);
-    scanf("%s",a.Author);
+    printf("sit No:");gotoxy(30,8);
+    scanf("%s",a.sit_number);
+
     gotoxy(21,9);
-    printf("Quantity:");gotoxy(31,9);
-    scanf("%d",&a.quantity);
+    printf("Room No:");gotoxy(31,9);
+    scanf("%d",&a.Room_No);
+
     gotoxy(21,10);
-    printf("Price:");gotoxy(28,10);
-    scanf("%f",&a.Price);
-    gotoxy(21,11);
-    printf("Rack No:");gotoxy(30,11);
-    scanf("%d",&a.rackno);
+    printf("Adv.TK:");
+    gotoxy(28,10);
+    scanf("%d",&a.Advance);
     return 1;
 }
+
+
+
 
 
 int checkid(int t)  //check whether the book is exist in library or not
@@ -357,44 +340,45 @@ int checkid(int t)  //check whether the book is exist in library or not
     return 1; //return 1 if it not
 }
 
+
+
 int t(void) //for time
-{
-    time_t t;
-    time(&t);
-    printf("Date and time:%s\n",ctime(&t));
+{}
 
-    return 0 ;
-}
 
-void Password(void) //for password option
+             //for password option
+
+void Password(void)
 {
-    gotoxy(10,10);
-    char d[25]="Login only admin";
+    system("cls");
+
+   // char d[25]="    BUBT University  ";
     char ch,pass[10];
     int i=0,j;
-
-    gotoxy(10,10);
+    gotoxy(15,7);
     printf("Enter Password:");
 
     while(ch!=13)
-        {
-            ch=getch();
+    {
+        ch=getch();
 
-            if(ch!=13 && ch!=8)
-                {
-                        putch('*');
-                        pass[i] = ch;
-                        i++;
-                }
+        if(ch!=13 && ch!=8)
+        {
+            putch('*');
+            pass[i] = ch;
+            i++;
         }
+    }
     pass[i] = '\0';
     if(strcmp(pass,password)==0)
     {
         mainmenu();
-    }else
-        {
-            gotoxy(15,16);
-            printf("\a Incorrect Password");
-
-        }
+    }
+    else
+    {
+        gotoxy(15,16);
+        printf("\t Incorrect Password !!!!!");
+        getch();
+        Password();
+    }
 }
